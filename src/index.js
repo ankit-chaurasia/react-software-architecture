c0987c89877ee2e4b5761f8b85598f2da2680617
Ankit Chaurasia <chaurasiaankit07@gmail.com>
2021-09-10 13:53:25 +0530

    Added Server Side rendering

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+// Takes pre rendered html from server and react to it. so that it can acts when data changes
+ReactDOM.hydrate(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
