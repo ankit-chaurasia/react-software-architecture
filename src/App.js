0f614bd8a3bad3709b37a284614ce9f7dd2f3b7d
Ankit Chaurasia <chaurasiaankit07@gmail.com>
2021-09-14 22:18:38 +0530

    useState hook

--- src/App.js
+++ src/App.js
@@ -4,40 +4,47 @@ import styled from 'styled-components';
 import { Home } from './pages/Home';
 import { About } from './pages/About';
 import { Articles } from './pages/Articles';
+import CounterButton from './pages/CounterButton';
 
 const BigGreenHeading = styled.h1`
-	color: green;
-	font-size: 96px;
+  color: green;
+  font-size: 96px;
 `;
 
 const App = () => {
-	return (
-		<>
-			<BigGreenHeading>Server-Side Rendering Example</BigGreenHeading>
-			<ul>
-				<li>
-					<Link to="/">Home</Link>
-				</li>
-				<li>
-					<Link to="/about">About</Link>
-				</li>
-				<li>
-					<Link to="/articles">Articles</Link>
-				</li>
-			</ul>
-			<Switch>
-				<Route path="/" exact>
-					<Home />
-				</Route>
-				<Route path="/about">
-					<About />
-				</Route>
-				<Route path="/articles">
-					<Articles />
-				</Route>
-			</Switch>
-		</>
-	);
-}
+  return (
+    <>
+      <BigGreenHeading>Server-Side Rendering Example</BigGreenHeading>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/articles">Articles</Link>
+        </li>
+        <li>
+          <Link to="/counterButton">Counter Button</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/articles">
+          <Articles />
+        </Route>
+        <Route path="/counterButton">
+          <CounterButton />
+        </Route>
+      </Switch>
+    </>
+  );
+};
 
 export default App;
